889d19dd193887542f4502f0810fd39f3c1658cf
JR Concepcion <137083455+JRCon1@users.noreply.github.com>
2025-03-17 21:27:14 -0400

    Update Dash-Final v1.py

--- Dash-Final v1.py
+++ Dash-Final v1.py
@@ -130,6 +130,7 @@ app = dash.Dash(__name__,
     meta_tags=[{'name': 'viewport',
                 'content': 'width=device-width, initial-scale=1.0'}]
 )
+server = app.server
 
 # Custom CSS for better styling
 app.index_string = '''
